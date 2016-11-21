d66c2802f606a800208931029cc5835965b2621b
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2016-11-21 13:44:57 +0100

    indexing of files works great

--- src/retrieval_system/FileIndexer.java
+++ src/retrieval_system/FileIndexer.java
@@ -1,10 +1,15 @@
 package retrieval_system;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
@@ -19,15 +24,38 @@ public class FileIndexer {
 		File f = new File(System.getProperty("java.class.path"));
 		File filePath = f.getAbsoluteFile().getParentFile();
 		String path = filePath.toString();
+		path = "c:\\Users\\hyperion\\Documents\\GitHub\\retrieval_system\\documents";
 
 		FSDirectory dir = FSDirectory.open(Paths.get(path));
 		IndexWriterConfig config = new IndexWriterConfig(mAnalyzer);
 
 		mWriter = new IndexWriter(dir, config);
 	}
+	
+	public void indexDirectory(String directory) throws IOException {
+		File dir = new File(directory);
+		for (File curFile: dir.listFiles()) {
+			String filename = curFile.getName().toLowerCase();
+			
+			if (curFile.isDirectory()) {
+				this.indexDirectory(curFile.getAbsolutePath());
+			}
+			else if (filename.endsWith(".txt") || filename.endsWith(".html")) {
+				Document doc = new Document();
+				doc.add(new TextField("content", new FileReader(curFile)));
+				doc.add(new StringField("path", curFile.getPath(), Field.Store.YES));
+				mWriter.addDocument(doc);
+				
+				System.out.println("Added: " + curFile.getAbsolutePath());
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		FileIndexer indexer = new FileIndexer();
-		
+		indexer.mWriter.deleteAll();
+		indexer.indexDirectory("c:\\Users\\hyperion\\Documents\\GitHub\\retrieval_system\\documents\\plain_text");
+		System.out.println("Indexed " + indexer.mWriter.numDocs() +" documents.");
+		indexer.mWriter.close();
 	}
 }
