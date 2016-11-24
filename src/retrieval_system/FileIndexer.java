910566455931a01c930931ec529ac98eba46fcae
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2016-11-24 16:21:12 +0100

    search done nigga

--- src/retrieval_system/FileIndexer.java
+++ src/retrieval_system/FileIndexer.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardFilterFactory;
@@ -34,19 +35,12 @@ import org.apache.lucene.analysis.en.PorterStemFilterFactory;
 
 public class FileIndexer {
 
+	private Analyzer mAnal = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class)
+			.addTokenFilter(StandardFilterFactory.class).addTokenFilter(LowerCaseFilterFactory.class)
+			.addTokenFilter(StopFilterFactory.class).addTokenFilter(PorterStemFilterFactory.class).build();
 
-	private static StandardAnalyzer analyzer = new StandardAnalyzer();
-	private Analyzer mAnal = CustomAnalyzer.builder()
-			.withTokenizer(StandardTokenizerFactory.class)
-			.addTokenFilter(StandardFilterFactory.class)
-			.addTokenFilter(LowerCaseFilterFactory.class)
-			.addTokenFilter(StopFilterFactory.class)
-			.addTokenFilter(PorterStemFilterFactory.class)
-		    .build();
-	
-	
 	private IndexWriter mWriter;
-	
+
 	private String indexPath = "";
 
 	FileIndexer() throws IOException {
@@ -57,38 +51,41 @@ public class FileIndexer {
 		path = "c:\\Users\\hyperion\\Documents\\GitHub\\retrieval_system\\documents";
 		indexPath = path;
 		FSDirectory dir = FSDirectory.open(Paths.get(path));
-		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		IndexWriterConfig config = new IndexWriterConfig(mAnal);
 
 		mWriter = new IndexWriter(dir, config);
 		mWriter.deleteAll();
 	}
-	
+
 	public void indexDirectory(String directory) throws IOException {
 		File dir = new File(directory);
-		for (File curFile: dir.listFiles()) {
-			String filename = curFile.getName().toLowerCase();
-			
+		for (File curFile : dir.listFiles()) {
 			if (curFile.isDirectory()) {
 				this.indexDirectory(curFile.getAbsolutePath());
-			}
-			else if (filename.endsWith(".txt") || filename.endsWith(".html")) {
-				Document doc = new Document();
-				doc.add(new TextField("content", new FileReader(curFile)));
-				doc.add(new StringField("path", curFile.getPath(), Field.Store.YES));
-				mWriter.addDocument(doc);
-				
-				System.out.println("Added: " + curFile.getAbsolutePath());
+			} else {
+				String filename = curFile.getName().toLowerCase();
+				if (filename.endsWith(".txt") || filename.endsWith(".html")) {
+					
+					Document doc = new Document();
+					doc.add(new TextField("contents", new FileReader(curFile)));
+					doc.add(new StringField("path", curFile.getPath(), Field.Store.YES));
+					doc.add(new StringField("filename", curFile.getName(), Field.Store.YES));
+					
+					mWriter.addDocument(doc);
+					System.out.println("Added: " + curFile.getAbsolutePath());
+				}
 			}
 		}
 	}
-	
+
+
 	public void search(String searchquery) throws IOException, ParseException {
 
-		IndexReader reader =  DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
 		IndexSearcher searcher = new IndexSearcher(reader);
 		TopScoreDocCollector collector = TopScoreDocCollector.create(5);
 
-		Query query = new QueryParser("contents", analyzer).parse(searchquery);
+		Query query = new QueryParser("contents", mAnal).parse(searchquery);
 		searcher.search(query, collector);
 		ScoreDoc[] scores = collector.topDocs().scoreDocs;
 
@@ -98,27 +95,27 @@ public class FileIndexer {
 			Document d = searcher.doc(docId);
 			System.out.println((i + 1) + ". " + d.get("path") + " score=" + scores[i].score);
 		}
-		
+
 	}
 
 	public static void main(String[] args) throws IOException, ParseException {
 		FileIndexer indexer = new FileIndexer();
 		indexer.indexDirectory("c:\\Users\\hyperion\\Documents\\GitHub\\retrieval_system\\documents\\plain_text");
-		System.out.println("Indexed " + indexer.mWriter.numDocs() +" documents.");
+		System.out.println("Indexed " + indexer.mWriter.numDocs() + " documents.");
 		indexer.mWriter.close();
-		
+
 		BufferedReader bufreader = new BufferedReader(new InputStreamReader(System.in));
 		String queryInput = "";
 		while (!queryInput.equalsIgnoreCase("yx")) {
-			
+
 			System.out.println("Search Query (yx = exit):");
-			queryInput  = bufreader.readLine();
-			
+			queryInput = bufreader.readLine();
+
 			if (queryInput.equalsIgnoreCase("yx")) {
 				break;
 			}
 			indexer.search(queryInput);
 		}
-		
+
 	}
 }
