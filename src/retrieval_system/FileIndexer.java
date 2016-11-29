bab62adb3c2862855ef99983b281dc88953bad28
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2016-11-29 19:01:36 +0100

    324234
    23424

--- src/retrieval_system/FileIndexer.java
+++ src/retrieval_system/FileIndexer.java
@@ -6,12 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.lucene.analysis.standard.StandardFilterFactory;
 import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.LegacyLongField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -24,13 +28,16 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.FSDirectory;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
 import org.apache.lucene.analysis.core.StopFilterFactory;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.en.PorterStemFilterFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+@SuppressWarnings("deprecation")
 public class FileIndexer {
 
 	private Analyzer mAnal = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class)
@@ -63,11 +70,40 @@ public class FileIndexer {
 			} else {
 				String filename = curFile.getName().toLowerCase();
 				if (filename.endsWith(".txt") || filename.endsWith(".html")) {
-					
 					Document doc = new Document();
-					doc.add(new TextField("contents", new FileReader(curFile)));
+					
+					if(filename.endsWith(".html")) {
+						org.jsoup.nodes.Document doc_jsoup = Jsoup.parse(curFile, "UTF-8", "");
+						Element sum = doc_jsoup.select("summary").first();
+						Element dat = doc_jsoup.select("time[datetime]").first();
+						
+						doc.add(new TextField("contents", doc_jsoup.text(), Field.Store.YES));
+						doc.add(new TextField("title", doc_jsoup.title(), Field.Store.YES));
+						if(sum != null){
+							doc.add(new TextField("summary", sum.text() , Field.Store.YES));
+						}
+						else{
+							doc.add(new TextField("summary", "", Field.Store.YES));
+						}
+						
+						if(dat != null){
+							doc.add(new TextField("date", dat.attr("datetime"), Field.Store.YES));
+						}
+						else{
+							doc.add(new TextField("date", "", Field.Store.YES));
+						}
+
+					}
+					else{
+						doc.add(new TextField("contents", new FileReader(curFile)));
+						doc.add(new TextField("title", "", Field.Store.YES));
+						doc.add(new TextField("summary", "", Field.Store.YES));
+						doc.add(new TextField("date", "", Field.Store.YES));
+					}
+					
 					doc.add(new StringField("path", curFile.getPath(), Field.Store.YES));
 					doc.add(new StringField("filename", curFile.getName(), Field.Store.YES));
+					doc.add(new LegacyLongField("lastmodified", curFile.lastModified(), Field.Store.YES));
 					
 					mWriter.addDocument(doc);
 					System.out.println("Added: " + curFile.getAbsolutePath());
@@ -85,20 +121,34 @@ public class FileIndexer {
 
 		Query query = new QueryParser("contents", mAnal).parse(searchquery);
 		searcher.search(query, collector);
+		query = new QueryParser("title", mAnal).parse(searchquery);
+		searcher.search(query, collector);
+		query = new QueryParser("date", mAnal).parse(searchquery);
+		searcher.search(query, collector);
+		
+		
 		ScoreDoc[] scores = collector.topDocs().scoreDocs;
-
-		System.out.println("Found " + scores.length + " hits.");
+		System.out.println("Found " + scores.length + " documents.");
+		
+		//rank, path, relevance score, last modification time
+		if(scores.length > 0) {
+			System.out.println("Place <|> Path <|> Score <|> Last Modified <|> Title <|> Summary");
+		}
 		for (int i = 0; i < scores.length; ++i) {
 			int docId = scores[i].doc;
 			Document d = searcher.doc(docId);
-			System.out.println((i + 1) + ". " + d.get("path") + " score=" + scores[i].score);
+			long datelong = Long.parseLong(d.get("lastmodified"));
+			String dateString = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date(datelong));
+			
+			System.out.println((i + 1) + ". <|> " + d.get("path") + " <|> " + scores[i].score 
+					+ " <|> " + dateString+ " <|> " + d.get("title") + " <|> " + d.get("summary"));
 		}
 
 	}
 
 	public static void main(String[] args) throws IOException, ParseException {
 		FileIndexer indexer = new FileIndexer();
-		indexer.indexDirectory("c:\\Users\\hyperion\\Documents\\GitHub\\retrieval_system\\documents\\plain_text");
+		indexer.indexDirectory("c:\\Users\\hyperion\\Documents\\GitHub\\retrieval_system\\documents");
 		System.out.println("Indexed " + indexer.mWriter.numDocs() + " documents.");
 		indexer.mWriter.close();
 
@@ -106,7 +156,7 @@ public class FileIndexer {
 		String queryInput = "";
 		while (!queryInput.equalsIgnoreCase("yx")) {
 
-			System.out.println("Search Query (yx = exit):");
+			System.out.println("\nSearch Query (yx = exit):");
 			queryInput = bufreader.readLine();
 
 			if (queryInput.equalsIgnoreCase("yx")) {
