0bcbf2f979dd26c11a47ae8bd4ee3956cc886caa
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2016-11-30 23:30:01 +0100

    end stuff

--- src/retrieval_system/FileIndexer.java
+++ src/retrieval_system/FileIndexer.java
@@ -38,27 +38,58 @@ import org.jsoup.nodes.Element;
 @SuppressWarnings("deprecation")
 public class FileIndexer {
 
+	/**
+	 * This is the Analyzer used for indexing and searching. It uses porter stemmer.
+	 */
 	private Analyzer mAnal = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class)
 			.addTokenFilter(StandardFilterFactory.class).addTokenFilter(LowerCaseFilterFactory.class)
 			.addTokenFilter(StopFilterFactory.class).addTokenFilter(PorterStemFilterFactory.class).build();
 
+	/**
+	 * This is a writer for the index.
+	 */
 	private IndexWriter mWriter;
 
+	/**
+	 * Contains the path to our index.
+	 */
 	private String indexPath = "";
 
+	/**
+	 * The constructor of our class, initializes needed variables.
+	 * <p> 
+	 * The constructor gets the absolute path to the .jar file being run
+	 * and sets it as the index directory. Also initializes other needed
+	 * members like the mWriter.
+	 */
+	
 	FileIndexer() throws IOException {
 		// getting filepath to our jar file
 		File f = new File(System.getProperty("java.class.path"));
 		File filePath = f.getAbsoluteFile().getParentFile();
-		String path = filePath.toString();
-		indexPath = path;
-		FSDirectory dir = FSDirectory.open(Paths.get(path));
+		indexPath = filePath.toString();
+		FSDirectory dir = FSDirectory.open(Paths.get(indexPath));
 		IndexWriterConfig config = new IndexWriterConfig(mAnal);
 
 		mWriter = new IndexWriter(dir, config);
 		mWriter.deleteAll();
 	}
 
+	/**
+	 * Indexes "all" files in the specified URI directory.
+	 * The directory argument must specify an absolute {@link URI}.
+	 * <p> 
+	 * This method crawls trough all folders in the specified directory
+	 * and takes all files ending with .TXT or .HTML and indexes them.
+	 * For HTML files it looks for the first occurrence of the tag
+	 * "summary" and saves the containing text in the datatype Document for the 
+	 * specific file. The same is done for the first occurrence of a "time" tag
+	 * with the attribute "datetime" e.g. &lt;time datetime="1914"&gt;
+	 * It then save the value of the attribute in the same manner as it is 
+	 * done for the summary tag
+	 *
+	 * @param  directory the URI to the directory which contains the files to be indexed
+	 */
 	public void indexDirectory(String directory) throws IOException {
 		File dir = new File(directory);
 		for (File curFile : dir.listFiles()) {
@@ -109,7 +140,18 @@ public class FileIndexer {
 		}
 	}
 
-
+	/**
+	 * Searches for the specified query in our created index.
+	 * The searchquery argument must specify a string to be looked for.
+	 * <p> 
+	 * This method looks for the specified string query in our index and prints out
+	 * all files containg this file with additional information.
+	 * For all files it returns the Rank, Path to the file, the Score 
+	 * and the time it was last modified.
+	 * Additionally for HTML files it returns the Title and a Summary.
+	 *
+	 * @param  searchquery the String which shall be looked for in the created index
+	 */
 	public void search(String searchquery) throws IOException, ParseException {
 
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
@@ -129,7 +171,7 @@ public class FileIndexer {
 		
 		//rank, path, relevance score, last modification time
 		if(scores.length > 0) {
-			System.out.println("Place <|> Path <|> Score <|> Last Modified <|> Title <|> Summary");
+			System.out.println("Rank <|> Path <|> Score <|> Last Modified <|> Title <|> Summary");
 		}
 		for (int i = 0; i < scores.length; ++i) {
 			int docId = scores[i].doc;
@@ -142,7 +184,17 @@ public class FileIndexer {
 		}
 
 	}
-
+	
+	/**
+	 * Creates an objects of our FileIndexer class and uses it for indexing and searching.
+	 * The args argument must contain the directory as absolute URI at first place where the files are that shall be indexed.
+	 * <p> 
+	 * Creates an object of the FileIndexer class, takes the directory from the args argument
+	 * and uses it with a method to index all files.
+	 * Then it takes input as a search query and uses a method to search for the query in our index.
+	 * 
+	 * @param  args the directory at first place where the files are that shall be indexed.
+	 */
 	public static void main(String[] args) throws IOException, ParseException {
 		FileIndexer indexer = new FileIndexer();
 		indexer.indexDirectory(args[0]);
